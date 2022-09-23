e60bdc5fc3645d77cd216e288a0cf6c07963517b
ValRab <Santiagorecoba@hotmail.es>
2022-09-23 15:02:24 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,10 @@ export default function Home({ songs }) {
         <meta name="description" content="See what songs are popular right now!" />
       </Head>
       <Header></Header>
-      <div className="mx-12 my-8">
+      <div className="flex flex-col gap-4 mt-4 pb-8">
         <span className="text-white font-bold text-4xl ml-10 p-4">Top 100</span>
 
-        <div className="p-4 mt-4">
+        <div>
           <SongGrid songs={songs}></SongGrid>
         </div>
       </div>
