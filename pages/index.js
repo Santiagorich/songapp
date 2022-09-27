6afd3e5cff810bfe49232152cfaa92bd2e97e3bd
ValRab <Santiagorecoba@hotmail.es>
2022-09-27 01:26:48 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home({ preload }) {
       <div className="flex flex-col gap-4 mt-4 mx-4 pb-8">
         <div className="flex flex-row px-6 py-2 overflow-hidden whitespace-nowrap relative fader select-none">
           <Swiper
-            slidesPerView={4}
+            slidesPerView={"auto"}
             spaceBetween={0}
             keyboard={{
               enabled: true,
@@ -85,7 +85,7 @@ export default function Home({ preload }) {
             allowTouchMove={true}
           >
             {categories.map((category, index) => (
-                <SwiperSlide key={index} className="flex justify-center">
+              <SwiperSlide key={index} className="flex justify-center">
                 <span
                   key={index}
                   className={`${
