a43fdc914ae9af3b7ab0bbddb07ae6c1f36c68a6
ValRab <Santiagorecoba@hotmail.es>
2022-09-27 01:01:36 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,11 @@ export async function getStaticProps() {
 export default function Home({ preload }) {
   const [currentlyPlaying, setCurrentlyPlaying] = React.useState(null);
   const [currentCategory, setCurrentCategory] = React.useState(preload);
+  const [volume, setVolume] = React.useState(1);
   const playSong = (song) => {
     const audio = new Audio(song);
     audio.play();
+    audio.volume = volume;
     setCurrentlyPlaying({
       src: song,
       audio: audio,
@@ -102,9 +104,54 @@ export default function Home({ preload }) {
         </div>
 
         <div>
-          <span className="text-white font-bold text-4xl ml-10 p-4">
-            {currentCategory.name}
-          </span>
+          <div className="flex flex-row mx-12">
+            <span className="text-white font-bold text-4xl p-4">
+              {currentCategory.name}
+            </span>
+            <div className="flex flex-row items-center ml-auto text-white gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 9.75L19.5 12m0 0l2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6l4.72-4.72a.75.75 0 011.28.531V19.94a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.506-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.395C2.806 8.757 3.63 8.25 4.51 8.25H6.75z"
+                />
+              </svg>
+
+              <input
+                type="range"
+                min="0"
+                max="1"
+                step="0.01"
+                defaultValue={volume}
+                className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                onChange={(e) => {
+                  setVolume(e.target.value);
+                  if (currentlyPlaying) {
+                    currentlyPlaying.audio.volume = e.target.value;
+                  }
+                }}
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
+                />
+              </svg>
+            </div>
+          </div>
           <SongSwiper
             playSong={playSong}
             pauseSong={pauseSong}
