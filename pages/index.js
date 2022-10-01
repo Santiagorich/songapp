109e9be69a2d414245fb6bd704d60b44390aad3b
ValRab <Santiagorecoba@hotmail.es>
2022-10-01 03:23:00 -0300

    Back to option 1

--- pages/index.js
+++ pages/index.js
@@ -112,6 +112,7 @@ export default function Home({ preload }) {
     categories.map(async (category) => {
        fetchSongs(category.category);
     });
+    console.log("Mounted");
     // categories.map(async (category) => {
     //   let songRes = await fetchSongs(category.category);
     //   await Promise.all(
