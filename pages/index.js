6255eb7f142b4e269c02b7bc630475a9d0acc428
ValRab <Santiagorecoba@hotmail.es>
2022-10-03 12:18:12 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -129,10 +129,10 @@ export default function Home({ preload, props }) {
   });
 
   useEffect(() => {
-    // if (!checked) {
-    //   dispatch(setMobile(checkMobile()));
-    //   dispatch(setChecked(true));
-    // }
+     if (!checked) {
+       dispatch(setMobile(checkMobile()));
+       dispatch(setChecked(true));
+     }
     if (window) {
       window.addEventListener("resize", () => {
         dispatch(setMobile(checkMobile()));
@@ -152,26 +152,26 @@ export default function Home({ preload, props }) {
     //   );
     // });
     
-    onAuthStateChanged(auth, (logInUser) => {
-      console.log("Login event", logInUser);
-      if (logInUser) {
-        const logUser = {
-          email: logInUser.email.toString(),
-          uid: logInUser.uid.toString(),
-          displayName: logInUser.displayName.toString(),
-          photoUrl: logInUser.photoURL.toString(),
-        };
-        console.log("User logging in", logUser);
-        set(ref(rtdb, "online/" + logUser.uid), logUser);
-        console.log("User logged in", logUser);
-        dispatch(setUser(logUser));
-      } else {
-        console.log("User logging out");
-        goOffline(user);
-        console.log("User logged out");
-        dispatch(setUser(null));
-      }
-    });
+   onAuthStateChanged(auth, (logInUser) => {
+    console.log("Login event", logInUser);
+    if (logInUser && !user) {
+      const logUser = {
+        email: logInUser.email.toString(),
+        uid: logInUser.uid.toString(),
+        displayName: logInUser.displayName.toString(),
+        photoUrl: logInUser.photoURL.toString(),
+      };
+      console.log("User logging in", logUser);
+      set(ref(rtdb, "online/" + logUser.uid), logUser);
+      console.log("User logged in", logUser);
+      dispatch(setUser(logUser));
+    } else {
+      console.log("User logging out");
+      goOffline(user);
+      console.log("User logged out");
+      dispatch(setUser(null));
+    }
+  });
   }, []);
   
   return (
