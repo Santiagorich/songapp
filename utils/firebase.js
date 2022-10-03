c5c750adaed77d44b15f0e21eac1ac73ff6dff2c
ValRab <Santiagorecoba@hotmail.es>
2022-10-03 01:47:15 -0300

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
   authDomain: "acvhelper-93c1a.firebaseapp.com",
   databaseURL: "https://acvhelper-93c1a-default-rtdb.firebaseio.com",
   projectId: "acvhelper-93c1a",
@@ -12,7 +12,6 @@ const firebaseConfig = {
   appId: "1:651177855214:web:d5b6c1d9b01fa77c900bff",
   measurementId: "G-7M0BE5XFG2"
 };
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
