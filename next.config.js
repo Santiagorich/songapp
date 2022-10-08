3cb61eebcc0b71be581f90f80b54213dd135bc2a
ValRab <Santiagorecoba@hotmail.es>
2022-10-08 12:30:45 -0300

    PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -7,9 +13,25 @@ const nextConfig = {
     defaultLocale: "en",
   },
   images: {
-    domains: ["is1-ssl.mzstatic.com", "is2-ssl.mzstatic.com", "is3-ssl.mzstatic.com", "is4-ssl.mzstatic.com", "is5-ssl.mzstatic.com", "is6-ssl.mzstatic.com", "is7-ssl.mzstatic.com", "is8-ssl.mzstatic.com", "is9-ssl.mzstatic.com", "is10-ssl.mzstatic.com", "is11-ssl.mzstatic.com", "is12-ssl.mzstatic.com", "is13-ssl.mzstatic.com", "is14-ssl.mzstatic.com", "is15-ssl.mzstatic.com"],
+    domains: [
+      "is1-ssl.mzstatic.com",
+      "is2-ssl.mzstatic.com",
+      "is3-ssl.mzstatic.com",
+      "is4-ssl.mzstatic.com",
+      "is5-ssl.mzstatic.com",
+      "is6-ssl.mzstatic.com",
+      "is7-ssl.mzstatic.com",
+      "is8-ssl.mzstatic.com",
+      "is9-ssl.mzstatic.com",
+      "is10-ssl.mzstatic.com",
+      "is11-ssl.mzstatic.com",
+      "is12-ssl.mzstatic.com",
+      "is13-ssl.mzstatic.com",
+      "is14-ssl.mzstatic.com",
+      "is15-ssl.mzstatic.com",
+    ],
     unoptimized: true,
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
