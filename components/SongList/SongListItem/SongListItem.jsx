22e24b402700298fd4a559cc503cfe23c72da329
ValRab <Santiagorecoba@hotmail.es>
2022-10-10 08:54:41 -0300

    Update SongListItem.jsx

--- components/SongList/SongListItem/SongListItem.jsx
+++ components/SongList/SongListItem/SongListItem.jsx
@@ -48,7 +48,24 @@ function SongListItem({ song, playSong, pauseSong, currentlyPlaying }) {
             {song.artist}
           </span>
         </div>
-
+{/* 
+        <div className=" w-8 h-8 text-white flex z-20">
+          {isCurrentSong && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-8 h-8"
+            >
+              <path
+                fillRule="evenodd"
+                d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z"
+                clipRule="evenodd"
+              />
+            </svg>
+          )}
+        </div>
+         */}
         <button
           className="ml-auto"
           onClick={(e) => {
@@ -83,22 +100,6 @@ function SongListItem({ song, playSong, pauseSong, currentlyPlaying }) {
             </svg>
           </div>
         </button>
-        <div className=" w-8 h-8 text-white flex z-20">
-          {isCurrentSong && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-8 h-8"
-            >
-              <path
-                fillRule="evenodd"
-                d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z"
-                clipRule="evenodd"
-              />
-            </svg>
-          )}
-        </div>
       </div>
     </div>
   );
